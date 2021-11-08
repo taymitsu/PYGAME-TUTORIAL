806267d82036a7b43ef0a10786518fba67559366
Tay <tmitsumori96@gmail.com>
2021-11-08 14:53:53 -0800

    first example

--- game.py
+++ game.py
@@ -10,27 +10,31 @@ screen = pygame.display.set_mode([500, 500])
 # Creat the game loop
 running = True 
 while running: 
-	# Looks at events 
+	#Looks at events 
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			running = False
-	
-	# Clear the screen
+	#Clear the screen
 	screen.fill((255, 255, 255))
-	# Draw a circle
-	color = (255, 0, 255)
-	position = (250, 250)
-	pygame.draw.circle(screen, color, position, 75)
-	# Update the display screen
+    #Draw circle
+	for i in range(0, 9):
+		color = (255, 0, 255)
+		x = ((i % 3) * 175) + 75
+		y = (int(i / 3) * 175) + 75
+		position = (x, y)
+		#Update screen display 
+		pygame.draw.circle(screen, color, position, 75)
 	pygame.display.flip()
 
-# Clear the screen
-screen.fill((255, 255, 255))
-
-# Your drawing here
-
-# Update the display
-pygame.display.flip()
+	
+	# Clear the screen
+	#screen.fill((255, 255, 255))
+	# Draw a circle
+	#color = (255, 0, 255)
+	#position = (250, 250)
+	#pygame.draw.circle(screen, color, position, 75)
+	# Update the display screen
+	#pygame.display.flip()
 
 #(255, 0, 0) - RED has no Green or Blue
 #(0, 255, 0) - GREEN has no Red or Blue
@@ -44,3 +48,7 @@ pygame.display.flip()
 #(250, 141, 7) - A nice shade of orange.
 #(25, 184, 22) - A nice shade of green.
 #(20, 121, 199) - A nice shade of green.
+#position = (250, 250) - Puts the circle in the center.
+#position = (0, 250) - Places the circle at the left edge. Since the pygame.draw.circle() method draws from the center the circle is half off the screen and its center is at the left edge.
+#position = (0, 0) - Place the center of the circle at the top left corner. left edge.
+#position = (400, 300) - Place the circle near the lower right.
